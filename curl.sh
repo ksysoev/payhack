cc067485ddb0f3c5658e0bb6078e949fc6025237
Lalitaditya Addanki <laddanki@outlook.com>
2023-05-27 22:24:19 +0800

    added express json app & adding MYSQL

---
+++ curl.sh
@@ -0,0 +1,3 @@
+## curl commands for all the manual DB changes go here:
+
+curl -X POST -H "Content-Type: application/json" -d '{"wallet_public_key":"asdf201" , "balance":"30" ,"currency":"MYR"}' http://localhost:3000/create
